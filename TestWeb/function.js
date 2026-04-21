fe9a4bdb622d4609fdb66d4bb6062a9707ed455a
Kit-is-inWorld <chokunzaza0021@gmail.com>
2026-04-21 11:14:06 +0700

    day 3

--- TestWeb/function.js
+++ TestWeb/function.js
@@ -1,1 +1,11 @@
-document.getElementById("title").textContent = "เปลี่ยนแล้ว";
+let count = 0;
+
+document.getElementById("plus").addEventListener("click", () => {
+  count++;
+  document.getElementById("count").textContent = count;
+});
+
+document.getElementById("minus").addEventListener("click", () => {
+  count--;
+  document.getElementById("count").textContent = count;
+});
